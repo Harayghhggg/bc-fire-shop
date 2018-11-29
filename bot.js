7b3eec30f86b0d80b5129a38f2e06465e93f5146
Harayghhggg <44177526+Harayghhggg@users.noreply.github.com>
2018-11-29 23:55:00 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,44 @@
-const Discord = require('discord.js');const client = new Discord.Client();var prefix = "*" client.on('ready', function() { console.log(`i am ready ${client.user.username}`);}); const developers = ["319624924769681412"]const adminprefix = "*";client.on('message', message => { var argresult = message.content.split(` `).slice(1).join(' '); if (!developers.includes(message.author.id)) return; if (message.content.startsWith(adminprefix + 'ply')) { client.user.setGame(argresult); message.channel.send(`**Now Playig ${argresult}**`) } else if (message.content === (adminprefix + "leaveserver")) { message.guild.leave(); } else if (message.content.startsWith(adminprefix + 'wt')) { client.user.setActivity(argresult, {type:'WATCHING'}); message.channel.send(`**Now Watching ${argresult}**`) } else if (message.content.startsWith(adminprefix + 'ls')) { client.user.setActivity(argresult , {type:'LISTENING'}); message.channel.send(`**Now Listening ${argresult}**`) } else if (message.content.startsWith(adminprefix + 'st')) { client.user.setGame(argresult, "https://www.twitch.tv/idk"); message.channel.send(`**Now Streaming ${argresult}**`) } if (message.content.startsWith(adminprefix + 'setname')) { client.user.setUsername(argresult).then message.channel.send(`Changing The Name To ..**${argresult}** `)} elseif (message.content.startsWith(adminprefix + 'setavatar')) { client.user.setAvatar(argresult); message.channel.send(`Changing The Avatar To :**${argresult}** `);}});  
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('======================================')
+    console.log(`by haray`);
+    console.log('=======================================')
+});
+const developers = ["314135031029170197"]
+const adminprefix = "-";
+client.on('message', message => {
+      var argresult = message.content.split(` `).slice(1).join(' ');
+        if (!developers.includes(message.author.id)) return;
+        
+    if (message.content.startsWith(adminprefix + 'pl')) {
+          client.user.setGame(argresult);
+            message.channel.send(`**Status You   ${argresult}**`)
+    } else 
+        if (message.content.startsWith(adminprefix + 'wt')) {
+            client.user.setActivity(argresult, {type:'WATCHING'});
+                message.channel.send(`**Status You   ${argresult}**`)
+        } else 
+            if (message.content.startsWith(adminprefix + 'ls')) {
+                client.user.setActivity(argresult , {type:'LISTENING'});
+                    message.channel.send(`**Status You  ${argresult}**`)
+            } else 
+                if (message.content.startsWith(adminprefix + 'st')) {
+                      client.user.setGame(argresult, "https://www.twitch.tv/kokoseda");
+                        message.channel.send(`**Status You ${argresult} **`)
+                }
+    if (message.content.startsWith(adminprefix + 'setname')) {
+        client.user.setUsername(argresult).then
+            message.channel.send(`Changing The Name To ..**${argresult}** `)
+    } else
+      if (message.content.startsWith(adminprefix + 'setavatar')) {
+          client.user.setAvatar(argresult);
+            message.channel.send(`Changing The Avatar To :**${argresult}** `);
+      }
+});
+
+
 
 
 
