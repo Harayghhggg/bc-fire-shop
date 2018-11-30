6a714cfc33fcf5df088d8f7cd474b4bcd4284df6
Harayghhggg <44177526+Harayghhggg@users.noreply.github.com>
2018-11-30 05:01:01 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,6 +110,31 @@ f!obc | لارسال برودكاست بمنشن
  });
  
 
+client.on('message',async message => {
+        if(message.author.bot || message.channel.type === 'f!2bc') return;
+        let args = message.content.split(' ');
+        if(args[0] === `*bc`) {
+                  if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- **       **');
+                  if(!args[1]) return message.channel.send('- **     **');
+               
+                  let msgCount = 0;
+                  let errorCount = 0;
+                  let successCount = 0;
+                  message.channel.send(`**- [ :bookmark: :: ${msgCount} ]   **\n**- [ :inbox_tray: :: ${successCount} ]   **\n**- [ :outbox_tray: :: ${errorCount} ]   **`).then(msg => {
+                              message.guild.members.forEach(g => {
+                                            g.send(args.slice(1).join(' ')).then(() => {
+                                                            successCount++;
+                                                            msgCount++;
+                                                            msg.edit(`**- [ :bookmark: :: ${msgCount} ]   **\n**- [ :inbox_tray: :: ${successCount} ]   **\n**- [ :outbox_tray: :: ${errorCount} ]   **`);
+                                            }).catch(e => {
+                                                            errorCount++;
+                                                            msgCount++;
+                                                            msg.edit(`**- [ :bookmark: :: ${msgCount} ]   المرسلة**\n**- [ :inbox_tray: :: ${successCount} ]   **\n**- [ :outbox_tray: :: ${errorCount} ]   **`);
+                                            });
+                              });
+                  });
+        }
+});
 
 
 client.login(process.env.BOT_TOKEN);
